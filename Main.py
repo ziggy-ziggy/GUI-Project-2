0f9f45835e3851841ef3da6549ae120294db4279
ziggy-ziggy <jacob.simondet@jacks.sdstate.edu>
2019-04-23 17:33:27 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,18 @@
+#**********************************************
+#Students   : Jacob, Mark, Viraj, Cole
+#Class      : Human Factors and User Interface
+#Instructor : Gamradt
+#Assignment : 4
+#Due Date   : 5/3/2019
+#**********************************************
+#Description: This is an implementaiton for Ken's
+Coffe and Bagels
+#**********************************************
+
+
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import messagebox, StringVar, OptionMenu
+from PIL import Image, ImageTk
+
+#Later
